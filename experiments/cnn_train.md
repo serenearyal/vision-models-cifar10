904db81a1bdc99c8cbc8ace178d66a7dc6162b17
Serene Aryal <serene.aryal24@gmail.com>
2026-02-20 09:51:50 -0600

    data augmentation; accuracy ~75%

--- experiments/cnn_train.md
+++ experiments/cnn_train.md
@@ -2,3 +2,6 @@
   - LR = 0.01
   - weight_decay = 1e-5
   - batch_size = 128
+
+- data augmentation (random crop & random horizontal flip):
+  - increased accuracy by ~3% to ~75%
--- experiments/cnn_train.py
+++ experiments/cnn_train.py
@@ -30,6 +30,8 @@ WEIGHT_DECAY = 0.00001
 
 
 transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
 ])
